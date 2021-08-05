1df0c16cb4ac665c7c73230c15fb49ec08d01ff5
Jorge Sira <jsirab@gmail.com>
2021-08-04 23:29:22 -0400

    Final Upload

---
+++ Instructions/Starter-Code/TieredProfitSplitter.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.5.0;
+
+// lvl 2: tiered split
+contract TieredProfitSplitter {
+    address payable employee_one; // ceo
+    address payable employee_two; // cto
+    address payable employee_three; // bob
+
+    constructor(address payable _one, address payable _two, address payable _three) public {
+        employee_one = _one;
+        employee_two = _two;
+        employee_three = _three;
+    }
+
+    // Should always return 0! Use this to test your `deposit` function's logic
+    function balance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function deposit() public payable {
+        uint points = msg.value / 100; // Calculates rudimentary percentage by dividing msg.value into 100 units
+        uint total;
+        uint amount;
+
+        // @TODO: Calculate and transfer the distribution percentage
+        // Step 1: Set amount to equal `points` * the number of percentage points for this employee
+        
+        // ceo gets 60%
+        amount = points * 60;
+
+        // Step 2: Add the `amount` to `total` to keep a running total
+
+        total += amount;
+
+        // Step 3: Transfer the `amount` to the employee
+
+        employee_one.transfer(msg.value - total); // ceo gets the remaining wei
+
+        // @TODO: Repeat the previous steps for `employee_two` and `employee_three`
+        
+        // cto gets 25%
+        amount = points * 25;
+        total += amount;
+        employee_two.transfer(amount);
+
+
+        // bob gets 15%         
+        amount = points * 15;
+        total += amount;
+        employee_three.transfer(amount);
+
+        
+        
+        employee_one.transfer(msg.value - total); // ceo gets the remaining wei
+    }
+
+    function() external payable {
+        deposit();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Solidity_Homework
 Looks like we've made our First Contract!
+
+## Profit Splitter Contract
+This will accept Ether into the contract and divide the Ether evenly among the associate level employees. This will allow the Human Resources department to pay employees quickly and efficiently
+
+WE are using Visual Studio Code' Remix plugin and Metamask to connect the contrat with our local blockchain using Ganache for testing porpuses.
+
+*The owner's acoount = *0xec449D5b1ca25b481621b7d20D7621737068De1b*
+
+The three adresses are taken from Ganache account as follows;
+
+* employee_one = *0x20C0fFcf54F8bCc1f550e6Ca5Ab7093ED452e983*
+* employee_two = *0x5ea1fcFdE6234Df8755b74AD5ac1443f0e687Daf*
+* employee_three = *0xE970e17E520C5643E3FE1ed814Cf7f7F53E6a7af*
+
+The employees's accounts hava an initial balance of $100
+
+
+
+
